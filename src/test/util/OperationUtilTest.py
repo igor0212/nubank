e3a8a4b3c1dd8a2467eec3ac63536ed5322067ed
Igor Oliveira <igor.oliveira.ufmg@gmail.com>
2025-08-13 22:07:36 -0300

    Creating unit tests

--- src/test/util/OperationUtilTest.py
+++ src/test/util/OperationUtilTest.py
@@ -4,11 +4,16 @@ from src.main.dto.OperationDto import OperationDto, OperationType
 
 class OperationUtilTest(unittest.TestCase):
     def test_format_operations_file_valid(self):
+        #Given
         lines = [
             '[{"operation":"buy", "unit-cost":10.00, "quantity": 10000}, {"operation":"sell", "unit-cost":20.00, "quantity": 5000}]',
             '[{"operation":"buy", "unit-cost":20.00, "quantity": 10000}]'
         ]
+
+        #When
         result = OperationUtil.formatOperationsFile(lines)
+
+        #Then
         self.assertEqual(len(result), 2)
         self.assertIsInstance(result[0][0], OperationDto)
         self.assertEqual(result[0][0].operation, OperationType.BUY)
@@ -17,19 +22,27 @@ class OperationUtilTest(unittest.TestCase):
         self.assertEqual(result[1][0].quantity, 10000)
 
     def test_format_operations_file_empty_lines(self):
+        #Given
         lines = [
             '',
             '   ',
             '[{"operation":"buy", "unit-cost":10.00, "quantity": 10000}]'
         ]
+
+        #When
         result = OperationUtil.formatOperationsFile(lines)
+
+        #Then
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0][0].operation, OperationType.BUY)
 
     def test_format_operations_file_invalid_json(self):
+        #Given
         lines = [
             '[{"operation":"buy", "unit-cost":10.00, "quantity": 10000}',
         ]
+
+        #Then
         with self.assertRaises(Exception):
             OperationUtil.formatOperationsFile(lines)
 
