f08a663aace29778a00db741872a249eb536c26d
Igor Oliveira <igor.oliveira.ufmg@gmail.com>
2025-08-13 22:38:11 -0300

    Refactoring methods

--- src/main/service/TaxService.py
+++ src/main/service/TaxService.py
@@ -6,19 +6,13 @@ from src.main.enum.OperationType import OperationType
 from src.main.dto.OperationDto import OperationDto
 from src.main.dto.OperationTaxDto import OperationTaxDto
 
-
 class TaxService:
     """Service for taxes."""    
 
     @staticmethod
     def calculateTaxes(operations: list[OperationDto]) -> list[OperationTaxDto]:
         """
-        Calculates the tax for each operation in the list, following the business rules:
-        - Tax is 20% over the profit of a sell operation, if the sell price > weighted average buy price.
-        - Weighted average price is recalculated on each buy.
-        - Losses are accumulated and used to offset future profits.
-        - No tax is paid if the total value of the sell operation (unit_cost * quantity) <= 20000.
-        - No tax is paid on buy operations.
+        Calculates the tax for each operation in the list, following the business rules.
         Returns:
             list: List of tax values (OperationTaxDto) for each operation.
         """
@@ -28,43 +22,108 @@ class TaxService:
         accumulated_loss = ZERO
 
         for op in operations:
-            if op.operation == OperationType.BUY:                
-                # Recalculate weighted average price
-                total_cost = weighted_avg * total_qty + op.unit_cost * op.quantity
-                total_qty += op.quantity
-                weighted_avg = total_cost / total_qty if total_qty > 0 else ZERO
-                taxes.append(OperationTaxDto(ZERO).toDict())
+            tax = ZERO
+            if op.operation == OperationType.BUY:
+                weighted_avg, total_qty = TaxService.__updateWeightedAvg(weighted_avg, total_qty, op)                
             elif op.operation == OperationType.SELL:
-                if op.quantity > total_qty:
-                    # Selling more than you have, treat as error or sell only what you have
-                    sell_qty = total_qty
-                else:
-                    sell_qty = op.quantity
-
-                total_qty -= sell_qty
-
-                total_value = op.unit_cost * sell_qty
-                profit = (op.unit_cost - weighted_avg) * sell_qty
-
-                # Deduct accumulated loss from profit if total operation value > 20000 
-                taxable_profit = profit
-                if accumulated_loss < 0 and total_value > TOTAL_VALUE_TRANSACTION:
-                    taxable_profit += accumulated_loss
-                    if taxable_profit > 0:
-                        accumulated_loss = ZERO
-                    else:
-                        accumulated_loss = taxable_profit
-                        taxable_profit = ZERO                
-
-                # No tax if total operation value <= 20000
-                if total_value <= TOTAL_VALUE_TRANSACTION or taxable_profit <= 0:
-                    if profit < 0:
-                        accumulated_loss += profit                    
-                    taxes.append(OperationTaxDto(ZERO).toDict())
-                else:
-                    tax = taxable_profit * TAX_PERCENTAGE                    
-                    taxes.append(OperationTaxDto(round(tax, 2)).toDict())
-            else:                
-                taxes.append(OperationTaxDto(ZERO).toDict())
+                tax, weighted_avg, total_qty, accumulated_loss = TaxService.__processSell(
+                    op, weighted_avg, total_qty, accumulated_loss
+                )                
+            
+            taxes.append(OperationTaxDto(tax).toDict())
 
         return taxes
+
+    @staticmethod
+    def __updateWeightedAvg(weighted_avg, total_qty, op: OperationDto):
+        """
+        Updates the weighted average cost and total quantity for buy operations.
+        Returns:
+            tuple: (weighted_avg, total_qty)
+        """
+        total_cost = weighted_avg * total_qty + op.unit_cost * op.quantity
+        total_qty += op.quantity
+        weighted_avg = total_cost / total_qty if total_qty > 0 else ZERO
+        return weighted_avg, total_qty
+
+    @staticmethod
+    def __processSell(op: OperationDto, weighted_avg, total_qty, accumulated_loss):
+        """
+        Processes a sell operation.
+        Returns:
+            tuple: (tax, weighted_avg, total_qty, accumulated_loss)
+        """
+        sell_qty = TaxService.__getSellQuantity(op.quantity, total_qty)
+        total_qty -= sell_qty
+        total_value = TaxService.__calculateTotalValue(op.unit_cost, sell_qty)
+        profit = TaxService.__calculateProfit(op.unit_cost, weighted_avg, sell_qty)
+
+        taxable_profit = profit
+        
+        # Should not deduct the profit obtained from accumulated losses if the total
+        # value of the transaction is less than or equal to 20000.00
+        if accumulated_loss < 0 and total_value > TOTAL_VALUE_TRANSACTION:
+            taxable_profit, accumulated_loss = TaxService.__deductAccumulatedLoss(taxable_profit, accumulated_loss)
+        tax, accumulated_loss = TaxService.__calculateTax(
+            total_value, taxable_profit, profit, accumulated_loss
+        )
+        return tax, weighted_avg, total_qty, accumulated_loss
+
+    @staticmethod
+    def __getSellQuantity(requested_qty, total_qty):
+        """
+        Validating if the selling quantity is greater than the total quantity
+        Returns:
+            int: The validated selling quantity
+        """        
+        return min(requested_qty, total_qty)
+
+    @staticmethod
+    def __calculateTotalValue(unit_cost, quantity):
+        """
+        Calculates the total value of a transaction.
+        Returns:
+            int: The total value of the transaction
+        """
+        return unit_cost * quantity
+
+    @staticmethod
+    def __calculateProfit(unit_cost, weighted_avg, quantity):
+        """
+        Calculates the profit for a given transaction.
+        Returns:
+            int: The profit for the transaction
+        """
+        return (unit_cost - weighted_avg) * quantity
+
+    @staticmethod
+    def __deductAccumulatedLoss(taxable_profit, accumulated_loss):
+        """
+        Deducts the accumulated loss from the profit if applicable.        
+        Returns:
+            tuple: (taxable_profit, accumulated_loss)
+        """        
+        taxable_profit += accumulated_loss
+        if taxable_profit > 0:
+            accumulated_loss = ZERO
+        else:
+            accumulated_loss = taxable_profit
+            taxable_profit = ZERO
+        return taxable_profit, accumulated_loss
+
+    @staticmethod
+    def __calculateTax(total_value, taxable_profit, profit, accumulated_loss):
+        """
+        Calculates the tax based on the provided parameters.
+        Returns:
+            tuple: (tax, accumulated_loss)
+        """
+        tax = ZERO
+        if total_value <= TOTAL_VALUE_TRANSACTION or taxable_profit <= 0:
+            if profit < 0:
+                accumulated_loss += profit
+        else:
+            tax = round(taxable_profit * TAX_PERCENTAGE, 2)
+        return tax, accumulated_loss
+
+
